7a6ed1edfb872bb9a881b85e9fc8d669d6438771
mikeforscutt <mikeforscutt@gmail.com>
2025-05-30 09:34:13 +0100

    Loading attendee relations

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -2,41 +2,39 @@
 
 namespace App\Http\Traits;
 
-use Illuminate\Database\Eloquent\Model ;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
-use \Illuminate\Database\Query\Builder as QueryBuilder;
-
 
 trait CanLoadRelationships
 {
-    public function loadRelationships(
-        Model | QueryBuilder | EloquentBuilder $for,
-        ?array $relations = null
-    ):Model|QueryBuilder|EloquentBuilder
-    {
+  public function loadRelationships(
+    Model|QueryBuilder|EloquentBuilder|HasMany $for,
+    ?array $relations = null
+  ): Model|QueryBuilder|EloquentBuilder|HasMany {
+    $relations = $relations ?? $this->relations ?? [];
 
-        $relations = $relations ?? $this->relations ?? [];
-
-        foreach ($relations as $relation) {
-            $for->when(
-                $this->shouldIncludeRelation($relation),
-                fn($q) => $for instanceof Model ? $for->load($relation) : $q->with($relation)
-            );
-        }
-
-        return $for;
+    foreach ($relations as $relation) {
+      $for->when(
+        $this->shouldIncludeRelation($relation),
+        fn($q) => $for instanceof Model ? $for->load($relation) : $q->with($relation)
+      );
     }
-    protected function shouldIncludeRelation(string $relation):bool
-    {
-        $include = request()->query('include');
-        
-        if(!$include) {
-            return false;
-        }
 
-        $relations = array_map('trim',explode(',', $include));
+    return $for;
+  }
 
-        return in_array($relation, $relations);
+  protected function shouldIncludeRelation(string $relation): bool
+  {
+    $include = request()->query('include');
 
+    if (!$include) {
+      return false;
     }
+
+    $relations = array_map('trim', explode(',', $include));
+
+    return in_array($relation, $relations);
+  }
 }
