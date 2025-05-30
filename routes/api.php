02dee87e881f61c0f7e7948f349672dc143a22b2
mikeforscutt <mikeforscutt@gmail.com>
2025-05-30 10:51:11 +0100

    Protecting routes and automatically adding tokens for user auth

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
